4b1f1790de0239568644ffdd30a431232df1996d
Raymond9734 <rymadara97@gmail.com>
2025-05-23 20:21:50 +0300

    feat: create index page
    This commit introduces the index page with a welcome message
    and a link to the about page.

---
+++ app/static/src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Link, usePage } from '@inertiajs/react';
+
+export default function Index() {
+    const { app_name } = usePage().props;
+
+    return (
+        <div>
+            <h1>{app_name}</h1>
+            <h1>Welcome to the Home Page</h1>
+            <Link href="/about">Abouadmdmowt</Link> 
+        </div>
+    );
+}
